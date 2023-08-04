4e894aaa82f4fd40700e5ee53dece429326d851a
Devanshu <devp65773@gmail.com>
2023-08-04 16:30:59 +0530

    test for rotate and move function

--- chandrayan.spec.js
+++ chandrayan.spec.js
@@ -1,5 +1,31 @@
-function testNavigateSpacecraft() {
-  //
+function rotate(currentDirection, command) {
+  const directions = {
+    N: { l: "W", r: "E", u: "U", d: "D" },
+    E: { l: "N", r: "S", u: "U", d: "D" },
+    W: { l: "S", r: "N", u: "U", d: "D" },
+    S: { l: "E", r: "W", u: "U", d: "D" },
+    U: { l: "W", r: "E", u: "S", d: "N" },
+    D: { l: "W", r: "E", u: "N", d: "S" },
+  };
+  return directions[currentDirection][command];
+}
+
+function move(currentPosition, currentDirection, val) {
+  if (
+    currentDirection == "S" ||
+    currentDirection == "W" ||
+    currentDirection == "D"
+  ) {
+    val *= -1;
+  }
+  if (currentDirection == "N" || currentDirection == "S") {
+    currentPosition.y += val;
+  } else if (currentDirection == "E" || currentDirection == "W") {
+    currentPosition.x += val;
+  } else {
+    currentPosition.z += val;
+  }
+  return currentPosition;
 }
 
 describe("Tests for navigateChandrayan arguments", () => {
@@ -43,6 +69,13 @@ describe("Test for executing commands", () => {
       }
     }
     expect(failed).toBe(false);
+  });
+});
+
+describe("Test for rotate and move functions", () => {
+  test("Test rotate functions", () => {
+    const currentDirection = "N";
+    expect(rotate(currentDirection, "l")).toBe("W");
   });
 });
 
